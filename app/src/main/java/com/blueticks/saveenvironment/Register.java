5939a76030efd4b9445d46a32257c096d95abff8
nmnsharma007 <nmnsharma20@gmail.com>
2021-08-29 22:04:02 +0530

    connected to firestore database

--- app/src/main/java/com/blueticks/saveenvironment/Register.java
+++ app/src/main/java/com/blueticks/saveenvironment/Register.java
@@ -2,21 +2,32 @@ package com.blueticks.saveenvironment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Objects;
 
 public class Register extends AppCompatActivity {
 
+    private static final String LOG_TAG = Register.class.getSimpleName();
     private TextInputEditText firstName;
     private TextInputEditText lastName;
     private TextInputEditText phnNumber;
     private Button regButton;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +38,7 @@ public class Register extends AppCompatActivity {
         lastName = findViewById(R.id.lastName);
         phnNumber = findViewById(R.id.phoneNumber);
         regButton = findViewById(R.id.registerButton);
-
+        db = FirebaseFirestore.getInstance();
         regButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,12 +59,36 @@ public class Register extends AppCompatActivity {
                 } else {
                     phnNumber.setError(null);
                 }
-                // get the phone number from the edit text view
+                // get the user details from the edit text view
                 String phoneNumber = phnNumber.getText().toString();
-                // Call the VerifyNumberClass and pass phone number to it
-                Intent intent = new Intent(Register.this, VerifyPhoneNumber.class);
-                intent.putExtra("phoneNumber", phoneNumber);
-                startActivity(intent);
+                String first_name = firstName.getText().toString();
+                String last_name = lastName.getText().toString();
+                db.collection("Users")
+                        .whereEqualTo("phoneNumber",phoneNumber)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                // Check if our task successfully communicated from the server
+                                if(task.isSuccessful()) {
+                                    if(task.getResult().size() == 0) {
+                                        // Call the VerifyNumberClass and pass phone number to it
+                                        Intent intent = new Intent(Register.this, VerifyPhoneNumber.class);
+                                        intent.putExtra("phoneNumber", phoneNumber);
+                                        intent.putExtra("firstName",first_name);
+                                        intent.putExtra("lastName",last_name);
+                                        intent.putExtra("isSignIn",false);// is the user being signed in
+                                        startActivity(intent);
+                                    }
+                                    else {
+                                        Toast.makeText(Register.this, "Phone Number already exists", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                                else{
+                                    Log.v(LOG_TAG,"There was an error while querying");
+                                }
+                            }
+                        });
             }
         });
 
--- app/src/main/java/com/blueticks/saveenvironment/VerifyPhoneNumber.java
+++ app/src/main/java/com/blueticks/saveenvironment/VerifyPhoneNumber.java
@@ -1,28 +1,26 @@
 package com.blueticks.saveenvironment;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.FirebaseTooManyRequestsException;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.concurrent.TimeUnit;
+
+import model.User;
 
 public class VerifyPhoneNumber extends AppCompatActivity {
     private static final String LOG_TAG = SignInActivity.class.getSimpleName();
@@ -30,35 +28,42 @@ public class VerifyPhoneNumber extends AppCompatActivity {
     private EditText phoneNumberEnteredByTheUser;
     private FirebaseAuth mAuth;
     private String verificationCodeBySystem;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private boolean oldUser;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_phone_number);
         verifyButton = findViewById(R.id.verify_btn);
-        verifyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String code = phoneNumberEnteredByTheUser.getText().toString();
-                if(code.isEmpty() || code.length() < 6) {
-                    phoneNumberEnteredByTheUser.setError("Wrong OTP...");
-                    phoneNumberEnteredByTheUser.requestFocus();
-                    return;
-                }
-                verifyCode(code);
+        verifyButton.setOnClickListener(v -> {
+            String code = phoneNumberEnteredByTheUser.getText().toString();
+            if(code.isEmpty() || code.length() < 6) {
+                phoneNumberEnteredByTheUser.setError("Wrong OTP...");
+                phoneNumberEnteredByTheUser.requestFocus();
+                return;
             }
+            verifyCode(code);
         });
         phoneNumberEnteredByTheUser = findViewById(R.id.verification_code_entered_by_user);
-        String phoneNumber = "+91" + getIntent().getStringExtra("phoneNumber");
+        firstName = getIntent().getStringExtra("firstName");
+        lastName = getIntent().getStringExtra("lastName");
+        phoneNumber = getIntent().getStringExtra("phoneNumber");
+        String phoneNumberWithCountry = "+91" + phoneNumber;
+        oldUser = getIntent().getBooleanExtra("isSignIn",false);
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
         // send the OTP as soon as this activity is started
-        sendVerificationCode(phoneNumber);
+        sendVerificationCode(phoneNumberWithCountry);
     }
 
-    private void sendVerificationCode(String phoneNumber) {
+    private void sendVerificationCode(String phoneNumberWithCountry) {
         // the method sends verification code
         PhoneAuthOptions options = PhoneAuthOptions.newBuilder(mAuth)
-                .setPhoneNumber(phoneNumber) // Phone number to verify
+                .setPhoneNumber(phoneNumberWithCountry) // Phone number to verify
                 .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
                 .setActivity(this) // Activity (for callback binding)
                 .setCallbacks(mCallbacks) // OnVerificationStateChangedCallbacks
@@ -67,7 +72,7 @@ public class VerifyPhoneNumber extends AppCompatActivity {
     }
 
     // the callback to detect the verification status
-    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+    private final PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
         @Override
         public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
             // Getting the code sent my SMS
@@ -110,20 +115,48 @@ public class VerifyPhoneNumber extends AppCompatActivity {
     private void signInTheUserByCredentials(PhoneAuthCredential credential) {
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(VerifyPhoneNumber.this,
-                        new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful()) {
+                        task -> {
+                            if(task.isSuccessful()) {
+                                if(!oldUser) {
+                                    // add this new user to that database
+                                    addDataToFireStore(firstName,lastName,phoneNumber);
+                                }
+                                else{
                                     Intent intent = new Intent(VerifyPhoneNumber.this,
                                             HomeActivity.class);
                                     startActivity(intent);
                                 }
-                                else {
-                                    Toast.makeText(VerifyPhoneNumber.this,
-                                            task.getException().getMessage(),
-                                            Toast.LENGTH_SHORT).show();
-                                }
+                            }
+                            else {
+                                Toast.makeText(VerifyPhoneNumber.this,
+                                        task.getException().getMessage(),
+                                        Toast.LENGTH_SHORT).show();
                             }
                         });
+    }
+
+    private void addDataToFireStore(String firstName,String lastName,String phoneNumber) {
+        // creating a collection reference
+        // for our Firebase Firestore database
+        CollectionReference dbUsers = db.collection("Users");
+        // Create the user object with the user data
+        User user = new User(firstName,lastName, phoneNumber);
+        // add the data to the database
+        dbUsers.
+            add(user).
+            addOnSuccessListener(documentReference -> {
+                // when the user has been added successfully, we display a message
+                Toast.makeText(VerifyPhoneNumber.this,"You have been registered successfully",Toast.LENGTH_SHORT).show();
+                // redirect the user to the home activity
+                Intent intent = new Intent(VerifyPhoneNumber.this,
+                        HomeActivity.class);
+                startActivity(intent);
+            }).addOnFailureListener(e -> {
+            // when user couldn't be added to the database
+            Toast.makeText(VerifyPhoneNumber.this,"An Error Occurred",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(VerifyPhoneNumber.this,
+                    SignInActivity.class);
+            startActivity(intent);
+        });
     }
 }
