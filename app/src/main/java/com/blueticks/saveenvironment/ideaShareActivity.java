6e47f0c69a39ccbdf5f601699e1f911568a9f07a
Anuraag Reddy <anuraagreddy74@outlook.com>
2021-08-29 23:25:12 +0530

    Completed backend for submitting ideas

--- app/src/main/java/com/blueticks/saveenvironment/ideaShareActivity.java
+++ app/src/main/java/com/blueticks/saveenvironment/ideaShareActivity.java
@@ -1,5 +1,6 @@
 package com.blueticks.saveenvironment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,13 +9,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
 import model.Idea;
 
 public class ideaShareActivity extends AppCompatActivity {
+
+    // Adding firebase stuff
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Ideas");
 
     private ArrayList<Idea> ideaArrayList;
     private RecyclerViewAdapter recyclerViewAdapter;
@@ -24,9 +40,11 @@ public class ideaShareActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_idea_share);
+
         ideaArrayList = new ArrayList<>();
-        Idea idea = new Idea("Kishor", "Gaddam", "No Idea as of now.....");
-        ideaArrayList.add(idea);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
@@ -53,5 +71,38 @@ public class ideaShareActivity extends AppCompatActivity {
         ideaArrayList.add(new Idea("Abcde", "Fghijk", idea));
         recyclerViewAdapter = new RecyclerViewAdapter(ideaShareActivity.this, ideaArrayList);
         recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        collectionReference
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if (!queryDocumentSnapshots.isEmpty()) {
+                            ideaArrayList.clear();
+                            for (QueryDocumentSnapshot ideas: queryDocumentSnapshots) {
+                                Idea idea = ideas.toObject(Idea.class);
+                                ideaArrayList.add(idea);
+                            }
+
+                            // Invoke recycler view
+                            recyclerViewAdapter = new RecyclerViewAdapter(ideaShareActivity.this,
+                                    ideaArrayList);
+                            recyclerView.setAdapter(recyclerViewAdapter);
+                            recyclerViewAdapter.notifyDataSetChanged();
+                        } else {
+
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
     }
 }
--- app/src/main/java/com/blueticks/saveenvironment/textOfIdea.java
+++ app/src/main/java/com/blueticks/saveenvironment/textOfIdea.java
@@ -1,15 +1,36 @@
 package com.blueticks.saveenvironment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import model.Idea;
+import util.UserApi;
 
 public class textOfIdea extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser user;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Ideas");
 
     private EditText ideaContent;
     private Button postBtn;
@@ -22,17 +43,40 @@ public class textOfIdea extends AppCompatActivity {
 
         ideaContent = findViewById(R.id.ideaContent);
         postBtn = findViewById(R.id.postButton);
-        cardsList = findViewById(R.id.cardsView);
 
         postBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // add text written by user to the database
-                Intent intent = new Intent(textOfIdea.this, ideaShareActivity.class);
-                intent.putExtra("addedIdea", ideaContent.getText().toString());
-                startActivity(intent);
+                saveIdea();
             }
         });
 
+    }
+
+    private void saveIdea() {
+        String firstName = UserApi.getInstance().getFirstName();
+        String lastName = UserApi.getInstance().getLastName();
+        String ideaFromUser = ideaContent.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(ideaFromUser)) {
+            Idea idea = new Idea(firstName, lastName, ideaFromUser);
+            collectionReference.add(idea)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            startActivity(new Intent(textOfIdea.this, ideaShareActivity.class));
+                            finish();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("Check", "onFailure: " + e.getMessage());
+                        }
+                    });
+        } else {
+            Toast.makeText(textOfIdea.this, "Please enter idea", Toast.LENGTH_SHORT).show();
+        }
     }
 }
