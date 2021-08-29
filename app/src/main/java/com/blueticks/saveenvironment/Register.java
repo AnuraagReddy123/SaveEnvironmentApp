124985d424b7998c7f268118694112fd830de4dd
nmnsharma007 <nmnsharma20@gmail.com>
2021-08-29 16:28:23 +0530

    Added firebase authentication

--- app/src/main/java/com/blueticks/saveenvironment/Register.java
+++ app/src/main/java/com/blueticks/saveenvironment/Register.java
@@ -1,32 +1,39 @@
 package com.blueticks.saveenvironment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
+
     private TextInputEditText firstName;
     private TextInputEditText lastName;
     private TextInputEditText phnNumber;
-    private TextInputEditText password;
     private Button regButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        // Initialise Firebase Auth
         firstName = findViewById(R.id.firstName);
         lastName = findViewById(R.id.lastName);
         phnNumber = findViewById(R.id.phoneNumber);
-        password = findViewById(R.id.password);
         regButton = findViewById(R.id.registerButton);
 
         regButton.setOnClickListener(new View.OnClickListener() {
@@ -49,12 +56,12 @@ public class Register extends AppCompatActivity {
                 }else{
                     phnNumber.setError(null);
                 }
-
-                if(password.getText().toString().trim().length() == 0){
-                    password.setError("Please enter password");
-                }else{
-                    password.setError(null);
-                }
+                // get the phone number from the edit text view
+                String phoneNumber = phnNumber.getText().toString();
+                // Call the VerifyNumberClass and pass phone number to it
+                Intent intent = new Intent(Register.this,VerifyPhoneNumber.class);
+                intent.putExtra("phoneNumber",phoneNumber);
+                startActivity(intent);
             }
         });
 
