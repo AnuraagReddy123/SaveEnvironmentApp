52c88c6da2b1aeff9a4798c9b7baa94072ddc994
Anuraag Reddy <anuraagreddy74@outlook.com>
2021-09-01 21:34:41 +0530

    Added notification activity

--- app/src/main/java/com/blueticks/saveenvironment/SettingsActivity.java
+++ app/src/main/java/com/blueticks/saveenvironment/SettingsActivity.java
@@ -2,13 +2,27 @@ package com.blueticks.saveenvironment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    private Switch notifSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        notifSwitch = findViewById(R.id.switch1);
+
+        notifSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
     }
 }
